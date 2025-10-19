69ea1f4e313513257b6c473d8689a01ea9c7a36d
andreykochetov <oliverr223344@gmail.com>
2025-10-19 14:58:46 +0600

    Task0

--- Tyuiu.KochetovAP.Sprint5.Task0.V25.Lib/DataService.cs
+++ Tyuiu.KochetovAP.Sprint5.Task0.V25.Lib/DataService.cs
@@ -9,10 +9,10 @@ namespace Tyuiu.KochetovAP.Sprint5.Task0.V25.Lib
         {
             double y = (3 * Math.Pow(x, 4) + 1) / Math.Pow(x, 3);
 
-            
-            string result = Math.Round(y, 3).ToString(CultureInfo.InvariantCulture);
 
-            
+            string result = Math.Round(y, 3).ToString(new CultureInfo("ru-RU"));
+
+
             string tempPath = Path.GetTempPath();
 
             
